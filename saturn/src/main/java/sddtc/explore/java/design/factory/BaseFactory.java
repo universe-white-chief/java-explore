a2fa268f4179c59b259b510f12ec5e2d3acf6959
sddtc <changhbaga@gmail.com>
2017-01-18 18:28:39 +0800

    add basefactory design

---
+++ saturn/src/main/java/sddtc/explore/java/design/factory/BaseFactory.java
@@ -0,0 +1,30 @@
+package sddtc.explore.java.design.factory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by hchang on 2017/1/18.
+ */
+public class BaseFactory {
+    private Map<String, Class<? extends UniverseInfo>> map;
+
+    public BaseFactory() {
+        map = new HashMap<>();
+        map.put("Sun", SunInfo.class);
+    }
+
+    public UniverseInfo create(String name) {
+        Class<? extends UniverseInfo> c = map.get(name);
+        UniverseInfo universeInfo = null;
+
+        try {
+            universeInfo = c.newInstance();
+        } catch (Exception ex) {
+
+        }
+
+        return universeInfo;
+    }
+
+}
---
+++ saturn/src/main/java/sddtc/explore/java/design/factory/UniverseInfo.java
@@ -0,0 +1,7 @@
+package sddtc.explore.java.design.factory;
+
+/**
+ * Created by hchang on 2017/1/18.
+ */
+public class UniverseInfo {
+}

1f96149435e0cabb7ed4b1460cecb7797ccdca8b
sddtc <changhbaga@gmail.com>
2017-01-25 09:25:22 +0800

    Finish Transactions And Trader

--- saturn/src/main/java/sddtc/explore/java/trader/TraderApp.java
+++ saturn/src/main/java/sddtc/explore/java/trader/TraderApp.java
@@ -1,9 +1,7 @@
 package sddtc.explore.java.trader;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
+import javax.swing.text.html.Option;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -26,6 +24,10 @@ public class TraderApp {
             new Transaction(alan, 2012, 950)
     );
 
+    /**
+     * 找出2011年的所有交易并按照交易额排序(从低到高)
+     * @return
+     */
     public List<Transaction> first() {
         List<Transaction> tr2011 = transactions.stream()
                 .filter(transaction -> transaction.getYear() == 2011)
@@ -34,15 +36,22 @@ public class TraderApp {
         return tr2011;
     }
 
+    /**
+     * 交易员都在哪些不同的城市工作过
+     * @return
+     */
     public Set<String> second() {
         Set<String> cities = transactions.stream()
                 .map(transaction -> transaction.getTrader().getCity())
-//                .distinct()
                 .collect(Collectors.toSet());
 
         return cities;
     }
 
+    /**
+     * 查找所有来自剑桥的交易员,按照姓名排序
+     * @return
+     */
     public List<Trader> third() {
         List<Trader> traders = transactions.stream()
                 .map(Transaction::getTrader)
@@ -54,6 +63,10 @@ public class TraderApp {
         return traders;
     }
 
+    /**
+     * 返回所有交易员的姓名字符串,按字母顺序排序
+     * @return
+     */
     public String fourth() {
         String names = transactions.stream()
                 .map(transaction -> transaction.getTrader().getName())
@@ -64,9 +77,48 @@ public class TraderApp {
         return names;
     }
 
-    public boolean five() {
+    /**
+     * 有没有交易员是在米兰工作的
+     * @return
+     */
+    public boolean fifth() {
         boolean milanBased = transactions.stream().anyMatch(transaction -> transaction.getTrader().getCity().equals("Milan"));
         return milanBased;
+    }
+
+    /**
+     * 打印生活在剑桥的交易员的交易额
+     */
+    public void sixth() {
+        transactions.stream()
+                .filter(transaction -> "Cambridge".equals(transaction.getTrader().getCity()))
+                .map(Transaction::getValue)
+                .forEach(System.out::println);
+
+    }
+
+    /**
+     * 所有交易中,最高的交易额是多少
+     * @return
+     */
+    public int seventh() {
+        Optional<Integer> highestValue = transactions.stream()
+                .map(Transaction::getValue)
+                .reduce(Integer::max);
+
+        return highestValue.get();
+
+    }
+
+    /**
+     * 找到交易额最小的交易
+     * @return
+     */
+    public Transaction eighth() {
+        Optional<Transaction> smallestTransaction = transactions.stream()
+                .reduce((t1, t2) -> t1.getValue()<t2.getValue()?t1:t2);
+
+        return smallestTransaction.get();
     }
 
 }
